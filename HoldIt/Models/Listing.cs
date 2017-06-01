22a46a5eadcc503ad5886ece3628548067087f7c
Karis Sponsler <karis4@pdx.edu>
2017-05-31 21:51:24 -0700

    added models

---
+++ HoldIt/Models/Listing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldIt.Models
+{
+    public class Listing
+    {
+        public Listing(DateTime time, double cost, int provider, String title, String descr, String addr, String state, String cntr)
+        {
+            this.datetime = time;
+            this.cost = cost;
+            this.providerID = provider;
+            this.description = descr;
+            this.title = title;
+            this.location = new Location(addr, state, cntr);
+            this.customerID = -1;
+        }
+        private DateTime datetime { get; }
+        private double cost { set; get; }
+        private int providerID { get; }
+        private int customerID { set; get; }
+        private String title { set; get; }
+        private String description { set; get; }
+        private Location location { set; get; }
+    }
+}
---
+++ HoldIt/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+
+namespace HoldIt.Models
+{
+    public class User
+    {
+        public User(String email, String name)
+        {
+            this.email = email;
+            this.name = name;
+            // TODO: add a way to generate ID from Database Schema
+        }
+        private String email { get; set; }
+        private String name { get; set; }
+        private int userID { get; }
+        private SortedList reviews;
+    }
+}
